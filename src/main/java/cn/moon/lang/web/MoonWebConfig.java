4ceee95194a275de289b978d50888c993194da09
jiangtao <410518072@qq.com>
2024-04-18 23:46:13 +0800

    增加工具类

--- src/main/java/cn/moon/lang/web/MoonWebConfig.java
+++ src/main/java/cn/moon/lang/web/MoonWebConfig.java
@@ -1,7 +1,13 @@
 package cn.moon.lang.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MoonWebConfig {
+
+    @Bean
+    public SpringTool springTool(){
+        return new SpringTool();
+    }
 }

95dedd76472f80c0fef245dc165a0c4945e3c091
moon <410518072@qq.com>
2024-04-30 22:08:51 +0800

    错误结果增加分类：异常500， 错误520

--- src/main/java/cn/moon/lang/web/Result.java
+++ src/main/java/cn/moon/lang/web/Result.java
@@ -13,8 +13,12 @@ import lombok.Setter;
 public class Result {
     public static final String ERROR_MESSAGE = "服务器忙";
 
+    public static final int OK = 200;
+    public static final int ERR_EXCEPTION = 500; // 异常
+    public static final int ERR_BIZ = 520; // 业务错误，比如一些提示
+
     boolean success = true;
-    int code = 200;
+    int code = OK;
     Object data = null;
     String message = "操作成功";
 
@@ -29,7 +33,7 @@ public class Result {
         Result rs = new Result();
         rs.setMessage(ERROR_MESSAGE);
         rs.setSuccess(false);
-        rs.setCode(500);
+        rs.setCode(ERR_BIZ);
         return rs;
     }
 

e365f8d446e6ec7eb310012e2ce621b29d337134
jiangtao <jiangtao.cctv@qq.com>
2024-07-03 08:47:41 +0800

    增加oneIndexed参数

--- src/main/java/cn/moon/lang/web/Pageable.java
+++ src/main/java/cn/moon/lang/web/Pageable.java
@@ -4,20 +4,40 @@ package cn.moon.lang.web;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Pageable {
 
     /**
      * 从 1 开始计算
      * indexed on one
      */
+    boolean oneIndexed = true;
 
-    int pageNo;
-    int pageSize;
+    int pageNo = 1;
+    int pageSize = 20;
 
+    public Pageable() {
+    }
+
+    public Pageable(int pageNo, int pageSize) {
+        this.pageNo = pageNo;
+        this.pageSize = pageSize;
+    }
+
+    public Pageable(boolean oneIndexed, int pageNo, int pageSize) {
+        this.oneIndexed = oneIndexed;
+        this.pageNo = pageNo;
+        this.pageSize = pageSize;
+    }
+
+    public void convertToZeroIndexed(){
+        if(this.oneIndexed){
+            pageNo = pageNo - 1;
+            this.oneIndexed = false;
+        }
+    }
 
     public static Pageable of(int page, int size) {
         return new Pageable(page, size);
